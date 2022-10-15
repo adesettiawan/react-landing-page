a4b3d2cc0ecfdaaea69fb2674df37dd9c0e33791
Ade Setiawan <adesetiawan0675@gmail.com>
2022-10-15 15:45:33 +0700

    add about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Intros from './components/intro/Intros';
+import React from "react";
+import About from "./components/about/About";
+import Intros from "./components/intro/Intros";
 
 function App() {
   return (
     <div>
-      <Intros/>
+      <Intros />
+      <About />
     </div>
   );
 }
